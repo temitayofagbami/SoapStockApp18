3a7cd9e46735e97e1eabc71bcd10fa6bf871779b
Temitayo Fagbami <temitayo.fagbami@gmail.com>
2018-04-07 06:56:28 -0700

    inital project

---
+++ SoapStockApp/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoapStockApp
+{
+    class Customer
+    {
+        public int CustomerID { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerEmail { get; set; }
+        public string CustomerAddr { get; set; }
+        public string CustomerPhone { get; set; }
+    }
+}
---
+++ SoapStockApp/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoapStockApp
+{
+    
+    class Item
+    {
+        #region Properties
+        public int ItemID { get; set; }
+        public string  ItemName { get; set; }
+        public string ItemType { get; set; }
+        public decimal ItemCost { get; set; }
+    }
+}
